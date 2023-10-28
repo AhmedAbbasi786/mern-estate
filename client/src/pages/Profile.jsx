b5c5efc2eb1aa0cfd05f21165be896297605daba
Bilal Ahmed Abbasi <babbasi38@gmail.com>
2023-10-28 15:51:43 +0500

    completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,72 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 export default function Profile() {
+  const {currentUser} = useSelector((state)=> state.user)
   return (
-    <div>
-        <h1>Profile</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form  className='flex flex-col gap-4'>
+        <input
+          type='file'
+          hidden
+          accept='image/*'
+        />
+         <img
+          src={currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+      
+        <input
+          type='text'
+          placeholder='username'
+        
+          id='username'
+          className='border p-3 rounded-lg'
+       
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          
+          className='border p-3 rounded-lg'
+        
+        />
+        <input
+          type='password'
+          placeholder='password'
+         
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+       
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span
+         
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
+
+     
+      <button  className='text-green-700 w-full'>
+        Show Listings
+      </button>
+     
+     
     </div>
-  )
+  );
 }
